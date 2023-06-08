3e1ee645e35c30d1f7a5f884377d280e6948f43b
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-08 09:22:19 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-import hidden_4
+if __name__ == "__main__":
 
-list_of_names = dir(hidden_4)
+    import hidden_4
 
-for name in list_of_names:
-    if name[:2] != "__":
-        print(name)
+    list_of_names = dir(hidden_4)
+
+    for name in list_of_names:
+        if name[:2] != "__":
+            print(name)
