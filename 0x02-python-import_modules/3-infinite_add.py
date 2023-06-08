ad68acafe570f75a7164d86c0c82a75ea5108e3a
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-08 09:20:58 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
 
-num_args = len(sys.argv) - 1
-res = 0
+    import sys
 
-for j in range(num_args):
-    res += int(sys.argv[j + 1])
-print("{}".format(res))
+    num_args = len(sys.argv) - 1
+    res = 0
+
+    for j in range(num_args):
+        res += int(sys.argv[j + 1])
+    print("{}".format(res))
