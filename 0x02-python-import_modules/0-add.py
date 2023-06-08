ca44bb11b1946482ae46a1099008e49a63aff48a
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-08 09:02:22 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 
-from add_0 import add
+    a = 1
+    b = 2
 
-a = 1
-b = 2
-
-res = add(a, b)
-print("{} + {} = {}".format(a, b, res))
+    res = add(a, b)
+    print("{} + {} = {}".format(a, b, res))
