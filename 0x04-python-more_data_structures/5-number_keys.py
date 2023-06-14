bc10cfc12aef29e213650d943355d2e5a44e9693
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-14 09:37:12 +0000

    5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
-    my_keys = list(a_dictionary.keys())
     results = 0
-    
+    my_keys = list(a_dictionary.keys())
+
     for num in my_keys:
         results += 1
     return(results)
