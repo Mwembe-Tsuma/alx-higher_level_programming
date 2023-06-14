20ae8826c15a3d9e85141975b1b3383697d657f6
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-14 10:00:46 +0000

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     
-    for k, v in a_dictionary.items():
-        new_dictionary[k] = v * 2
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value * 2
     return(new_dictionary)
