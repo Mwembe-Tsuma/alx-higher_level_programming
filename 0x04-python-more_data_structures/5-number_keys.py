254a8bf1776faf38388ffd160ca13d5b8727c5a2
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-14 08:52:35 +0000

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    my_keys = list(a_dictionary.keys())
+    results = 0
+    
+    for num in my_keys:
+        results += 1
+    return(results)
