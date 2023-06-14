4b8fb0cfca83bb2eae960d81dc84cfb7018a7e08
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-14 11:58:59 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,12 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
+
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     prev_val = 0
 
-    for num in range(len(roman_string) -1, -1, -1):
+    for num in range(len(roman_string) - 1, - 1, - 1):
         curr_val = rom.get(roman_string[num], 0)
 
         if curr_val >= prev_val:
@@ -16,4 +17,4 @@ def roman_to_int(roman_string):
             res -= curr_val
 
         prev_val = curr_val
-    return (res)
+    return(res)
