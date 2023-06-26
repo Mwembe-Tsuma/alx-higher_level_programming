ac33306ffacab8a9015175a7583beac7de7e4e2a
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-26 18:25:34 +0000

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,10 +2,11 @@
 
 import sys
 
+
 def safe_function(fct, *args):
     try:
         res = fct(*args)
         return (res)
     except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception:{}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
