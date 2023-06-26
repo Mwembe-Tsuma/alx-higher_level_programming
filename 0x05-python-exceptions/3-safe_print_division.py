61ed68a1d691f8fab28229b18be6d984e475a6b0
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-26 17:34:28 +0000

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         results = a / b
-    except(TypeError, ZeroDivisionError):
+    except (TypeError, ZeroDivisionError):
         results = None
     finally:
         print("Inside result: {}".format(results))
