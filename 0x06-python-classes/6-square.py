2f161940467d5ba4d461502190e33cd6cbec7bde
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-27 14:27:14 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 
 class Square:
     """Representation of a square."""
-    def __init__(self, size=0, position=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize an instance of a square.
         Args:
             size(int)
