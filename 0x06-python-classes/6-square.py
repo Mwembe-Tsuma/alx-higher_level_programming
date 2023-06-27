41552b1e5309f7d363e2f22f57f67f3123c9ad27
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-27 14:24:40 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,5 +56,3 @@ class Square:
             [print(" ", end="") for k in range(0, self.__position[0])]
             [print("#", end="") for m in range(self.__size)]
             print("")
-        if self.__size == 0:
-            print("")
