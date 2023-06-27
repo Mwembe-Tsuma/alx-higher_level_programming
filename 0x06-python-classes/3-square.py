ace9604978b653ce8ab40c72586dd7aae70e0c4d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-27 13:12:11 +0000

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,5 +15,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
-        """Method to return the area of the square."""
+        """Return area of the square."""
         return (self.__size * self.__size)
