e01a9b248e86301f6007a06bb5ac945b095ed5bc
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-27 12:42:01 +0000

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defining an empty class."""
+"""Define a class Square."""
+
 
 class Square:
-    """Representation of a class square."""
+    """Represent a square."""
     pass
