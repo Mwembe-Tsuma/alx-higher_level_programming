57033ea851ea1f389a9ff53236ae55104a6fe66d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-27 12:49:35 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defination a class Square."""
+
+
+class Square:
+    """Representation of a square."""
+    def __init__(self, size):
+        """Initialize an instance of a square.
+        Args:
+            size(int)
+        """
+        self.__size = size
