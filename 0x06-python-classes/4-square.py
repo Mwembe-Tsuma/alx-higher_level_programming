5812fa2057cf19f24399687308a056717f2f515e
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-27 13:58:20 +0000

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defination a class Square."""
+
+
+class Square:
+    """Representation of a square."""
+    def __init__(self, size=0):
+        """Initialize an instance of a square.
+        Args:
+            size(int)
+        """
+        self.zie = size
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return (self.__size)
+
+    @size_setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Method to return the current area of the square."""
+        return (self.__size * self.__size)
