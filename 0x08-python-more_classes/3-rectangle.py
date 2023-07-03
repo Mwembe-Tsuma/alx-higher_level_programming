5ca0480e6f3c82a3504079c6b8600cdd9d6ab621
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-03 10:37:03 +0000

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Define a Rectangle Class."""
+
+
+class Rectangle:
+    """Rep a Rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width.
+            height (int): The height.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get and set the width of the rect."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get and set the height of the rect."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Find the area of the Rect."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Find the perimeter of the Rect."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return (2 * ((self.__width) + (self.__height)))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return("")
+
+        rect = ""
+        for _ in range(self.height):
+            rect += "#" * self.__width + "\n"
+        return(rect)
