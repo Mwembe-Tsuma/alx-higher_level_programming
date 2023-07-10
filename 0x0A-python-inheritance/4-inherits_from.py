9bd2821cf4de3d6973a0a5f5180935e7f6cce611
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-10 12:46:49 +0000

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Define a func to check instance"""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of a class that
+    inherited (directly or indirectly)
+
+    Args:
+        obj (any): The object
+        a_class (type): The calss
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return (True)
+    return (False)
