29520b74da5cf444da87ab277218042971228da3
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-10 11:50:18 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,6 @@
 
 class Mylist(list):
     """Implement sorted print for the built in list class."""
-
     def print_sorted(self):
         """Prints list in ascending sort."""
         print(sorted(self))
