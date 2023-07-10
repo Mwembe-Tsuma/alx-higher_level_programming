89f2f0ee29d473e3d8cea3b7c4ad6867ba35d622
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-10 17:54:44 +0000

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,5 +12,5 @@ class Square(Rectangle):
             size (int): Square size.
         """
         self.integer_validator("size", size)
-        super().__init__(size, sieze)
+        super().__init__(size, size)
         self.__size = size
