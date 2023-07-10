4b515f559dac4d08f5919e8feed67f59007659c2
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-10 12:55:19 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Representation of class BaseGeometry."""
+    def area(self):
+        """Instance method to raise execption area() is not implemented."""
+        raise Exception("area() is not implemented")
