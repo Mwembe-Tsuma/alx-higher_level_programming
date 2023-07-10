2685f1eca94044064dcdb0ef22b991eba1cd8d20
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-10 10:57:14 +0000

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Define a lookup function"""
+
+def lookup(obj):
+    """Func to returns the list of available attributes
+    and methods of an object:
+    """
+    return (dir(obj))
