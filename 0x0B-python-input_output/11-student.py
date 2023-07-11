b8728e0e22951eb9743da6d56487616de73f0f92
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 14:56:59 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define class Student"""
+
+
+class Student:
+    """Rep a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new student.
+
+        Args:
+            first_name (str): First name
+            last_name (str): Last name
+            age (int): Age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Dict representation of a student"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student."""
+        for key, value in json.items():
+            setattr(self, key, value)
