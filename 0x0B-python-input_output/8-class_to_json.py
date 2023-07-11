6306a2138b32febbb47008aaedef5e2ad1c71a4b
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 14:25:31 +0000

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defination of a class to JSON function"""
 
+
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure"""
     return obj.__dict__
