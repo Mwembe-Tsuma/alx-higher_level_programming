1a09b8ce0cf111f0d95dc7645431457f753d98ca
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 15:22:06 +0000

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define line insertion function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append a new line after each line containg a given string.
+    
+    Args:
+        search_string (str): String to search
+        new_string (str): new string to append
+    """
+    txt = ""
+    with open(filename) as fd:
+        for line in fd:
+            txt += line
+            if search_string in line:
+                txt += new_string
+    with open(filename, "w") as f:
+        f.write(txt)
