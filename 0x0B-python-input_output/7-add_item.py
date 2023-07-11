6c7afd7d73a07d94ef97313e07323a004f47c741
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 13:08:50 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Define a script that add all args to a Python list and save them to a file."""
+"""Define a script that add all args to a
+    Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
