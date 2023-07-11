8b41eca095cb9c1c1a88975023b7b279cb4ad518
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 14:40:49 +0000

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define class Student"""
+
+
+def __init__(self, first_name, last_name, age):
+    """Initialize a new student.
+
+    Args:
+        first_name (str): First name
+        last_name (str): Last name
+        age (int): Age
+    """
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+def to_json(self):
+    """ Dict representation of a student"""
+    return self.__dict__
