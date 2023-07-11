d4a811bd9392b52c7d7e1154196971dbb409ae1c
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 10:45:34 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a func that reads contents of a file"""
+
+
+def read_file(filename=""):
+    """Function to read a file."""
+    with open(filename, "r", encoding="utf-8") as file:
+        fd = file.read()
+        print(fd)
