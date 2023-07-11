6608af7be7d6454b6f790bbb37483c95fe177691
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 11:00:18 +0000

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Define a func that append to of a text file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Function that append to a text file."""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
