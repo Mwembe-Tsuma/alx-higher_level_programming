ae435ed1d2211d48283257f5a687a54bd9093646
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 13:05:28 +0000

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a script that add all args to a Python list and save them to a file."""
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        item_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item_list = []
+    item_list.extend(sys.argv[1:])
+    save_to_json_file(item_list, "add_item.json")
