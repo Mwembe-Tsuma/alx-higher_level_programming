374c837fa8f4cfe0c1608db730230eb01c4800fe
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-11 14:43:42 +0000

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,18 +2,19 @@
 """Define class Student"""
 
 
-def __init__(self, first_name, last_name, age):
-    """Initialize a new student.
+class Student:
+    """Rep a Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new student.
 
-    Args:
-        first_name (str): First name
-        last_name (str): Last name
-        age (int): Age
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-
-def to_json(self):
-    """ Dict representation of a student"""
-    return self.__dict__
+        Args:
+            first_name (str): First name
+            last_name (str): Last name
+            age (int): Age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """ Dict representation of a student"""
+        return self.__dict__
