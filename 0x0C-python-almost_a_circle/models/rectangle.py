c127201c6e4c69260b1e1f8864d01d119391617c
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-17 10:08:27 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -131,4 +131,4 @@ class Rectangle(Base):
     def __str__(self):
         """String method."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format
-	    (self.id, self.x, self.y, self.width, self.height)
+        (self.id, self.x, self.y, self.width, self.height)
