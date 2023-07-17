03f33acecffbddc0d868b8e0931793f427cf733f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-17 19:38:38 +0000

    Square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -63,5 +63,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """String Method."""
-        return "[Square] ({}) {}/{} - {}".format
-        (self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
