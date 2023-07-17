370970f5f143f482edf737c0cefbd11b6854885e
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-07-17 08:51:01 +0000

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-"""Defines a class Base."""
-import csv
-import json
+"""Defines Base Class."""
 import turtle
+import json
+import csv
 
 
 class Base:
-    """Representaion of a Base Class model.
+    """Rep of a Base Class model.
 
     Attributes:
         __nb_object (int): No of instantiated Bases.
@@ -28,15 +28,15 @@ class Base:
             self.id = Base.__nb_objects
 
     @staticmethod
-    def into_json_string(list_dictionary):
+    def into_json_string(list_dict):
         """JSON serialization of a list of dictionaries.
 
         Args:
             list_dictionary (list): List of dicts.
         """
-        if list_dictionary is None or list_dictionary == []:
+        if list_dict is None or list_dict == []:
             return "[]"
-        return json.dumps(list_dictionary)
+        return json.dumps(list_dict)
 
     @classmethod
     def save_into_file(cls, list_objs):
@@ -50,7 +50,7 @@ class Base:
             if list_objs is None:
                 jsonfile.write("[]")
             else:
-                list_dicts = [o.to_dictionary() for o in list_objs]
+                list_dicts = [o.to_dictionary() for ob in list_objs]
                 jsonfile.write(Base.into_json_string(list_dicts))
 
     @staticmethod
@@ -135,35 +135,35 @@ class Base:
             list_rectangles (list):list of Rectangle objs to make.
             list_squares (list):list of Square objs to make.
         """
-        turt = turtle.Turtle()
-        turt.screen.bgcolor("#b7312c")
-        turt.pensize(3)
-        turt.shape("turtle")
+        tur = turtle.Turtle()
+        tur.screen.bgcolor("#b7312c")
+        tur.pensize(3)
+        tur.shape("turtle")
 
-        turt.color("#ffffff")
+        tur.color("#ffffff")
         for rec in list_rectangles:
-            turt.showturtle()
-            turt.up()
-            turt.goto(rec.x, rec.y)
-            turt.down()
+            tur.showturtle()
+            tur.up()
+            tur.goto(rec.x, rec.y)
+            tur.down()
             for idx in range(2):
-                turt.forward(rec.width)
-                turt.left(90)
-                turt.forward(rec.height)
-                turt.left(90)
-            turt.hideturtle()
+                tur.forward(rec.width)
+                tur.left(90)
+                tur.forward(rec.height)
+                tur.left(90)
+            tur.hideturtle()
 
-        turt.color("#b5e3d8")
+        tur.color("#b5e3d8")
         for sqr in list_squares:
-            turt.showturtle()
-            turt.up()
-            turt.goto(sqr.x, sqr.y)
-            turt.down()
+            tur.showturtle()
+            tur.up()
+            tur.goto(sqr.x, sqr.y)
+            tur.down()
             for idx in range(2):
-                turt.forward(sqr.width)
-                turt.left(90)
-                turt.forward(sqr.height)
-                turt.left(90)
-            turt.hideturtle()
+                tur.forward(sqr.width)
+                tur.left(90)
+                tur.forward(sqr.height)
+                tur.left(90)
+            tur.hideturtle()
 
         turtle.exitonclick()
