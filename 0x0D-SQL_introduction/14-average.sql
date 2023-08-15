3128dbde65824f3b1002fda92fc25e7c2d7c0c47
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-08-15 14:14:38 +0000

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- a script that computes the score average of all records in the table second_table
 
-SELECT AVARAGE(score) AS AVARAGE FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
