26cbd36ae35ba93208fab5f6d62a1410b49bc9fe
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-08-16 18:18:34 +0000

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,8 +3,8 @@
 SELECT name, SUM(rate) AS rating
   FROM tv_genres AS gen
        INNER JOIN tv_show_genres AS sh
-       ON sh.genre_id = gen.id
+       ON sh.genre_id = geb.id
        INNER JOIN tv_show_ratings AS rt
        ON rt.show_id = sh.show_id
  GROUP BY name
- ORDER BY rating` DESC;
+ ORDER BY rating DESC;
