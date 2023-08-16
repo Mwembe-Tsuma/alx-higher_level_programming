53eb067d6a386ef327ae62e0e605d96f6d016358
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-08-16 18:16:06 +0000

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,11 +1,10 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate
 
-SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS gen
-       INNER JOIN `tv_show_genres` AS show
-       ON show.`genre_id` = gen.`id`
-
-       INNER JOIN `tv_show_ratings` AS rt
-       ON rt.`show_id` = show.`show_id`
- GROUP BY `name`
- ORDER BY `rating` DESC;
+SELECT name, SUM(rate) AS rating
+  FROM tv_genres AS gen
+       INNER JOIN tv_show_genres AS sh
+       ON sh.genre_id = gen.id
+       INNER JOIN tv_show_ratings AS rt
+       ON rt.show_id = sh.show_id
+ GROUP BY name
+ ORDER BY rating` DESC;
