a9c38fd923474dfbdf5e20466fd20ec478982c2d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-11 19:36:54 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,8 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
   const count = Number(process.argv[2]);
-  for (let i = 0; i < count; i++){
+
+  for (let i = 0; i < count; i++) {
     console.log('C is fun');
   }
 }
