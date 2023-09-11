25e147c33375e0e6f0acb2c39cb9239c1a264cdb
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-11 20:24:30 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arry = process.argv.slice(2).map(Number);
-  const sec_big = arry.sort(function (a, b) { return b - a; })[1];
-  console.log(sec_big);
+  const sec = arry.sort(function (a, b) { return b - a; })[1];
+  console.log(sec);
 }
