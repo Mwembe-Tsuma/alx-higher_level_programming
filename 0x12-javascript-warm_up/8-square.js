29fc0f6ea811b5c421277e3303bd0a334c83f764
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-11 19:40:35 +0000

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  const count = Number(process.argv[2]);
+
+  for (let i = 0; i < count; i++) {
+    console.log('X'.repeat(x));
+  }
+}
