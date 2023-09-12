dc2baa2bc4cda98d8216726f6f6563f621fea9d9
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-12 13:18:38 +0000

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const New_Square = require('./5-rectangle');
+
+class Square extends New_Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let rows = 0; rows < this.height; rows++) {
+      let x = '';
+      for (let cols = 0; cols < this.width; cols++) {
+        x += c;
+      }
+      console.log(x);
+    }
+  }
+}
+
+module.exports = Square;
