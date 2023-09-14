cc8149a6c8343481c47e7f9744ca4b0d9ce7c47d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-14 18:14:25 +0000

    13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that adds the State object Louisiana
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    st_del = session.query(State).filter(State.name.like('%a%')).all()
+    for delete in st_del:
+        session.delete(delete)
+    session.commit()
+    session.close()
