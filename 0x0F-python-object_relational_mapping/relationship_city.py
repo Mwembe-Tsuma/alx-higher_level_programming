f40abf6405666f2e6d9b12f00d2d3adf3c57f503
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-15 12:38:42 +0000

    relationship_city.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
 """
     Module that creates a States class.
