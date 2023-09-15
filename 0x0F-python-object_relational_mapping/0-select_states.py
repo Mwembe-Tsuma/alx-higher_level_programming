38ee05e8f32d973e551c59953d86a1324981297f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-15 10:47:55 +0000

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Script that lists all states from db """
+
 import MySQLdb
 from sys import argv
 
@@ -9,7 +10,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     rows = cur.fetchall()
     for r in rows:
