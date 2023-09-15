a084bf5d32e65a742197aef3dad82b11a5abdbaf
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-15 12:07:23 +0000

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     s = session.query(State).filter(State.name.like('%a%'))\
-                                 .order_by(State.id).all()
+        .order_by(State.id).all()
 
     for state in s:
         print("{}: {}".format(state.id, state.name))
