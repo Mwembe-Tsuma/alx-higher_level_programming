40fdcbabeef96c0241cd9b87072b92c5b7762bc5
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-18 06:36:39 +0000

    relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,17 +2,13 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
-"""
-    Module that creates States class.
-"""
+"""Module that creates States class."""
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-        Class representing the states table
-    """
+    """Class representing the states table"""
     __tablename__ = 'states'
     id = Column(Integer, nullable=False, primary_key=True,
                 autoincrement=True, unique=True)
