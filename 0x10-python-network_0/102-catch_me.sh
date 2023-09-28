7e277100079a215575c2c5c6a7363f9d46c7fc6b
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-28 18:07:16 +0000

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -sLX PUT -d "user_id=98" -H "You got me!" 0.0.0.0:5000/catch_me
+#Bash script that makes a request to 0.0.0.0:5000/catch_me You got me
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
