7aea46732f76025e80fbe88c2c31555ea4ddb2eb
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-29 10:50:27 +0000

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+"""
+
+
+from urllib import request
+from urllib import parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = parse.urlencode({"email": argv[2]}).encode('utf-8')
+    with request.urlopen(argv[1], data) as response:
+        print(response.read().decode('utf-8'))
