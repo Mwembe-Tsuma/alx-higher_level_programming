162f7fd6a5c131305bfd77e2390af84345fb8185
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-29 09:18:25 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ url = "https://alx-intranet.hbtn.io/status"
 
 if __name__ == "__main__":
     with request.urlopen(url) as response:
-        content = response:.read()
+        content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
