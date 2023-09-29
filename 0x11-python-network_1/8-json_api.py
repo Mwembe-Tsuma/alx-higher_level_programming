c66b802194f0edf24050d90d70b4762b2362af63
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-29 12:04:32 +0000

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,20 +7,14 @@ http://0.0.0.0:5000/search_user
 import requests
 from sys import argv
 
-letter = sys.argv[1] if len(sys.argv) > 1 else ""
-
-url = "http://0.0.0.0:5000/search_user"
-params = {"q": letter}
-
-page = requests.post(url, params=params)
-
-try:
-    data = page.json()
-    if data:
-        print(f"[{data.get('id')}] {data.get('name')}")
-    else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
-except Exception as e:
-    print(f"An error occurred: {e}")
+if __name__ == "__main__":
+    data = {"q": argv[1] if len(argv) > 1 else ""}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        json = response.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
