946a664ed3a5294974358925681cf7fb99d7d6b2
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-29 11:11:00 +0000

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    print(requests.get(argv[1]).headers.get("X-Request-Id"))
