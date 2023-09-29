4f34aedb8deba574099f2d8f7478d9788846c43c
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-29 12:37:13 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,16 +6,9 @@ and uses the GitHub API to display your id
 
 
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    username, access_token = sys.argv[1], sys.argv[2]
-    url = "https://api.github.com/user"
-    page = requests.get(url, auth=(username, access_token))
-
-    if page.status_code == 200:
-        user_data = page.json()
-        user_id = user_data.get("id")
-        print(f"Your GitHub user ID: {user_id}")
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
+    auth = (argv[1], argv[2])
+    response = requests.get("https://api.github.com/user", auth=auth)
+    print(response.json().get("id"))
