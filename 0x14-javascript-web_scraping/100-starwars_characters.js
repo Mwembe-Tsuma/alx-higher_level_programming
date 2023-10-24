9b44deb6232566a9d8d5bdbdeee611ec6169cc2d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-24 13:31:06 +0000

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: Status Code ${response.statusCode}`);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      /*console.log(`Characters in ${movieData.title}:`);*/
+      movieData.characters.forEach((characterUrl) => {
+        request(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error(charError);
+          } else if (charResponse.statusCode !== 200) {
+            console.error(`Error: Status Code ${charResponse.statusCode}`);
+          } else {
+            const characterData = JSON.parse(charBody);
+            console.log(characterData.name);
+          }
+        });
+      });
+    } catch (parseError) {
+      console.error('Error parsing API response:', parseError);
+    }
+  }
+});
