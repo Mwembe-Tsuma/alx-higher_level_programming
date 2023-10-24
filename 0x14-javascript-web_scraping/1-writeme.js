669700626a9f00a3754c909fd4bf400e25f3f256
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-24 12:01:15 +0000

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,18 +2,8 @@
 
 const fs = require('fs');
 
-if (process.argv.length < 4) {
-  console.error('Usage: node your-script.js <file-path> <string-to-write>');
-  process.exit(1);
+try {
+  fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, result) { if (err) console.log(err); });
+} catch (err) {
+  console.log(err);
 }
-
-const path = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(path, content, 'utf8', function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Successfully wrote to ${path}`);
-  }
-});
